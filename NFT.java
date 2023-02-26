b7a57973f022cbee5df35bade477f53cdc43bd8d
EricaDC <desiree2009@att.net>
2023-02-26 11:56:48 -0500

    verifyNFT 1.0
    rough placeholders. pre functionality.

---
+++ NFT.java
@@ -0,0 +1,19 @@
+//To be replaced by NFT database
+
+public class NFT{
+    String name;
+    int price; 
+    boolean lock_status;
+
+
+
+public NFT(String name, int price, boolean lock_status){
+    this.name= name;
+    this.price=price;
+    this.lock_status=lock_status;
+}
+
+
+}
+
+
---
+++ manageNFT.java
@@ -0,0 +1,23 @@
+
+public class manageNFT{
+   
+        NFT NFT1 = new NFT("Mike",100, 1); 
+
+        
+    
+
+
+public boolean verifyNFT(NFT NFT1){ // if NFT is locked, return 1, else return 2. 
+    if (this.lock_status =1){
+        System.out.println("1");
+    } else{
+        System.out.println("2");
+    }
+    
+ }
+
+}
+
+
+
+ 
