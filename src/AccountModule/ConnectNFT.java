5ca327021f50164a49c9186cd9d70c78fa424de9
Ruhuan-Liao <73553116+Ruhuan-Liao@users.noreply.github.com>
2023-03-29 15:54:55 -0400

    connect nft action updated

--- src/AccountModule/ConnectNFT.java
+++ src/AccountModule/ConnectNFT.java
@@ -57,11 +57,6 @@ public class ConnectNFT extends JFrame {
         setMinimumSize(new Dimension(550, 550));
         setResizable(false);
 
-//        contentPane = new JPanel();
-//        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-//        setContentPane(contentPane);
-//        contentPane.setLayout(null);
-
         // Create a JPanel to hold the content
         JPanel panel = new JPanel(new GridBagLayout());
         panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10)); // Add some padding
@@ -99,6 +94,68 @@ public class ConnectNFT extends JFrame {
             public void actionPerformed(ActionEvent e) {
                 // database action
 
+                String token = getTokenFromUser(); // gets the token from the user
+                try {
+
+                    System.out.println("NFT connection name " + name);
+                    System.out.println("Connecting");
+
+
+                    Connection con = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/login_demo",
+                            "root", "root");
+                    System.out.println("user id: " + getUserId(con, name));
+
+                    // search for the token in the nftTokens table
+                    String query = "SELECT * FROM nftTokens WHERE NFT_Token=?";
+
+                    PreparedStatement stmt = con.prepareStatement(query);
+                    stmt.setString(1, token);
+                    ResultSet rs = stmt.executeQuery();
+
+                    if (rs.next()) {
+                        // token exists, modify the NFTs table owner_id to the user's id
+                        int nftId = rs.getInt("id");
+                        int userId = getUserId(con, name); // gets the user's id
+//
+                        String checkOwnerQuery = "SELECT owner_id FROM NFTs WHERE id=?";
+                        PreparedStatement checkOwnerStmt = con.prepareStatement(checkOwnerQuery);
+                        checkOwnerStmt.setInt(1, nftId);
+                        ResultSet checkOwnerRs = checkOwnerStmt.executeQuery();
+
+                        if (checkOwnerRs.next()) {
+                            int ownerId = checkOwnerRs.getInt("owner_id");
+                            if (ownerId == 0) {
+                                // NFT owner is null, update owner_id to the user's id
+                                String updateQuery = "UPDATE NFTs SET owner_id=? WHERE id=?";
+                                PreparedStatement updateStmt = con.prepareStatement(updateQuery);
+                                updateStmt.setInt(1, userId);
+                                updateStmt.setInt(2, nftId);
+                                int rowsAffected = updateStmt.executeUpdate();
+                                if (rowsAffected > 0) {
+                                    System.out.println("NFT ownership updated successfully.");
+                                    JOptionPane.showMessageDialog(connectButton, "NFT ownership updated successfully.");
+                                } else {
+                                    System.out.println("Error updating NFT ownership.");
+                                    JOptionPane.showMessageDialog(connectButton, "Error updating NFT ownership.");
+                                }
+                            } else {
+                                System.out.println("NFT already has an owner.");
+                                JOptionPane.showMessageDialog(connectButton, "NFT already has an owner.");
+
+                            }
+                        } else {
+                            System.out.println("Error checking NFT owner.");
+                        }
+                    } else {
+                        System.out.println("Token not found.");
+                        JOptionPane.showMessageDialog(connectButton, "Token not found.");
+
+                    }
+                } catch (SQLException ex) {
+                    System.out.println("Error searching for token: " + ex.getMessage());
+                }
+
+
             }
         });
 
@@ -113,5 +170,29 @@ public class ConnectNFT extends JFrame {
 
     }
 
-}
 
+    private int getUserId(Connection con, String name) {
+        // replace this with code that gets the user's id from the user_account table
+
+        int userId = -1;
+        try {
+            String query = "SELECT id FROM user_account WHERE name=?";
+            PreparedStatement stmt = con.prepareStatement(query);
+            stmt.setString(1, name);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                userId = rs.getInt("id");
+            }
+        } catch (SQLException ex) {
+            System.out.println("Error getting user ID: " + ex.getMessage());
+        }
+        return userId;
+    }
+
+    private String getTokenFromUser() {
+        // replace this with code that gets the token from the user
+        return textField.getText();
+    }
+
+
+}
