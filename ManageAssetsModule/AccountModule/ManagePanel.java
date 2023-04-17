b1d30c41c281132b69c6883e53c656f71e756fde
Rica <desiree2009@att.net>
2023-04-16 22:17:38 -0400

    Swing implementation
    Removed placeholder NFT.java file. Swing implementation of add/remove NFT

---
+++ ManageAssetsModule/AccountModule/ManagePanel.java
@@ -0,0 +1,72 @@
+package ManageAssetsModule.AccountModule;
+
+import ManageAssetsModule.manageNFT;
+
+import javax.swing.*;
+import javax.swing.event.AncestorListener;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ManagePanel extends JPanel {
+    private JLabel managePanelLabel;
+    private JButton manageLockNFTLabel;
+    private JButton manageUnLockLabel;
+
+    private JTextArea textField;
+    private JTextField textField1;
+
+    //private JLabel NFTLabel;
+
+    public ManagePanel(String name)
+    {
+        // Manage panel label
+        managePanelLabel = new JLabel("Manage your NFTs", SwingConstants.CENTER);
+        managePanelLabel.setFont(new Font("Times", Font.BOLD, 18));
+        add(managePanelLabel);
+
+        manageLockNFTLabel = new JButton("Lock NFTs");
+        manageLockNFTLabel.setFont(new Font("Times", Font.BOLD, 18));
+        add(manageLockNFTLabel);
+
+        manageUnLockLabel = new JButton("UnLock NFTs");
+        manageUnLockLabel.setFont(new Font("Times", Font.BOLD, 18));
+        add(manageUnLockLabel);
+
+        textField = new JTextArea();
+        textField.setText("");
+        add(textField);
+
+        manageLockNFTLabel.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String s = manageNFT.Showtable();
+                textField.setText(s);
+                String text = JOptionPane.showInputDialog("Type name of NFT to lock");
+                manageNFT.lockNFT(text);
+                s = manageNFT.Showtable();
+                textField.setText(s);
+                //System.out.println("Name of NFT to lock: "+text);
+            }
+        });
+
+        manageUnLockLabel.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                String s = manageNFT.Showtable();
+                textField.setText(s);
+                String text = JOptionPane.showInputDialog("Type name of NFT to lock");
+                manageNFT.unlockNFT(text);
+                s = manageNFT.Showtable();
+                textField.setText(s);
+            }
+        });
+        /*tring nftData = "";
+        NFTLSabel = new JLabel(nftData, SwingConstants.CENTER);
+        NFTLabel.setFont(new Font("Times", Font.BOLD, 18));
+        add(NFTLabel);*/
+    }
+}
--- ManageAssetsModule/manageNFT.java
+++ ManageAssetsModule/manageNFT.java
@@ -10,7 +10,8 @@ public class manageNFT {
         Scanner Console = new Scanner(System.in);
         String choice = "";
         while (choice.equals("0") == false) {
-            System.out.println("1. Show Table\n2. Lock NFT\n3. UnLock NFT\n0. Close Program.");
+            System.out.println(
+                    "1. Show Table\n2. Lock NFT\n3. UnLock\n4. Toggle Lock State\n5. Show Table\n0. Close Program.");
             choice = Console.nextLine();
             switch (choice) {
                 case "1":
@@ -22,62 +23,19 @@ public class manageNFT {
                 case "3":
                     unlockNFT();
                     break;
+                case "4":
+                    Button btn = new Button();
+                    btn.push();
+                    break;
+                case "5":
+                    Showtable();
+                    break;
             }
         }
-
-        /**
-         * Connection conn = null;
-         * try {
-         * String myConnectionString =
-         * "jdbc:mysql://localhost:3306?" +
-         * "useUnicode=yes&characterEncoding=UTF-8";
-         * conn = DriverManager.getConnection(myConnectionString, "root", "taco");
-         * Statement stmt = conn.createStatement();
-         * stmt.execute("SHOW DATABASES");
-         * ResultSet rs = stmt.getResultSet();
-         * while (rs.next()) {
-         * System.out.println(rs.getString(1));
-         * }
-         * rs.close();
-         * stmt.close();
-         * conn.close();
-         * } catch (SQLException ex) {
-         * ex.printStackTrace();
-         * }
-         * }
-         **/
     }
 
-    /**
-     * 
-     * //NFT NFT1 = new NFT("Magic_Mike", 100, true);
-     * 
-     * public void printNFT() {
-     * 
-     * 
-     * System.out.println("-------------------------");
-     * }
-     * 
-     * 
-     * public boolean verifyNFT(NFT NFT1) { // if NFT is locked, return 1, else
-     * return 2.
-     * if (NFT1.lock_status == true) {
-     * return true;
-     * } else {
-     * return false;
-     * }
-     * 
-     * }
-     * 
-     * public boolean LockNFT(NFT NFT1) {
-     * this.NFT1.lock_status = false;
-     * return (this.NFT1.lock_status);
-     * }
-     * 
-     * 
-     **/
-
-    public static void Showtable() {
+    public static String Showtable() {
+        String data = "";// Data was created to hold values from the NFT
         try {
             // connectToDataBase();
             // System.out.println("Connection Sucessful!.......");
@@ -99,12 +57,16 @@ public class manageNFT {
                 System.out.println("Creator: " + creator);
                 System.out.println("Locked: " + verifyNFT(locked, Name));
                 System.out.println("-------------------");
-
+                data += "Name: " + Name + "\n";// Stores values in the NFT
+                data += "Creator: " + creator + "\n";
+                data += "Locked: " + verifyNFT(locked, Name) + "\n";
+                data += "\n";
             }
             statement.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
+        return data;
     }
 
     public static boolean verifyNFT(String locked, String Name) {
@@ -130,11 +92,26 @@ public class manageNFT {
         connectToDataBase(com);
     }
 
+    public static void lockNFT(String nftName) {
+        System.out.println("Type the name of the nft you want to lock");
+        String com = "update nfttokens set lock_status = 1 where name = \"" + nftName + "\";";
+        System.out.println(com);
+        connectToDataBase(com);
+    }
+
     public static void unlockNFT()// Set to 0 if locked
     {
         System.out.println("Type the name of the nft you want to unlock");
         Scanner console = new Scanner(System.in);
         String nftName = console.nextLine();
+        String com = "update nfttokens set lock_status = 0 where name = \"" + nftName + "\";";
+        System.out.println(com);
+        connectToDataBase(com);
+    }
+
+    public static void unlockNFT(String nftName)// Set to 0 if locked
+    {
+        System.out.println("Type the name of the nft you want to unlock");
         String com = "update nfttokens set lock_status = 0 where name = \"" + nftName + "\";";
         System.out.println(com);
         connectToDataBase(com);
@@ -146,9 +123,6 @@ public class manageNFT {
                     "taco");
             System.out.println("Connection Sucessful!.......");
 
-            Statement statement = connection.createStatement();
-
-            // ResultSet resultSet = statement.executeQuery("select * from nfttokens");
             PreparedStatement state = connection.prepareStatement(com);
             state.executeUpdate();
             state.close();
